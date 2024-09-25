37bb7bdec37b7b745846df031eaa6852cb432962
Balázs Bencze <balazs.bencze@geogebra.org>
2024-09-25 18:55:24 +0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import purgecss from 'astro-purgecss';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://himamis.github.io',
+  base: 'varstins',
+  integrations: [purgecss()]
+});
