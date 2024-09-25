c9e976bc44471fcc57ecf82153a9202b35f0279a
Balázs Bencze <balazs.bencze@geogebra.org>
2024-09-25 21:53:28 +0300

    Trying to optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import purgecss from 'astro-purgecss';
+import playformCompress from '@playform/compress';
+
+import playformInline from '@playform/inline';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://himamis.github.io',
   base: 'varstins',
-  integrations: [purgecss()]
+  integrations: [playformInline({}), playformCompress()]
 });
