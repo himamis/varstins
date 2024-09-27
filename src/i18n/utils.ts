dacffab5d13c5aafb980a9bc295556244708732a
Balázs Bencze <balazs.bencze@geogebra.org>
2024-09-27 22:19:22 +0300

    Add images, update title

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,8 @@
 import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
+  const path = url.pathname.split('/');
+  const lang = path[path.length - 1];
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
