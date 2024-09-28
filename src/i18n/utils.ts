e8f69ac0c703a0aaede13a1a6bc75b578461bc0f
Balázs Bencze <balazs.bencze@geogebra.org>
2024-09-28 20:05:38 +0300

    Add facebook, langauge selector

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,7 @@
 import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
-  const path = url.pathname.split('/');
+  const path = url.pathname.split('/').filter((path) => path != "");
   const lang = path[path.length - 1];
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
